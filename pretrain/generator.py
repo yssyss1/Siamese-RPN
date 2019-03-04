943f17348cbf5c98e6a5ea8f0cd12442c7bb4985
seok <seok11754@gmail.com>
2019-03-05 02:31:14 +0900

    alexnet

--- pretrain/generator.py
+++ pretrain/generator.py
@@ -4,10 +4,11 @@ from glob import glob
 import os
 from pretrain.utils import load_image, normalize
 import cv2
+from imgaug import augmenters as iaa
 
 
 class BatchGenerator(Sequence):
-    def __init__(self, folder_path, batch_size, image_shape, shuffle=True):
+    def __init__(self, folder_path, batch_size, image_shape, shuffle=True, augmentation=True):
         super(BatchGenerator, self).__init__()
         self.folder_path = folder_path
         self.batch_size = batch_size
@@ -21,6 +22,28 @@ class BatchGenerator(Sequence):
 
         if self.num_data < self.batch_size:
             raise ValueError('Batch size should be lower than data size')
+
+        self.augment = augmentation
+        self.aug_pipe = iaa.Sequential(
+            [
+                iaa.SomeOf((0, 3),
+                           [
+
+                               iaa.Affine(translate_px={"x": (-12, 12), "y": (-12, 12)}),
+                               iaa.Affine(rotate=(-10, 10)),
+                               iaa.Affine(scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}),
+                               iaa.Add((-10, 10)),
+                               iaa.OneOf([
+                                   iaa.GaussianBlur((0, 1.0)),
+                                   iaa.AverageBlur(k=(0, 2.0)),
+                                   iaa.MedianBlur(k=(1.0, 3.0)),
+                               ]),
+                               iaa.AdditiveGaussianNoise(scale=(0, 0.05 * 255))
+                           ],
+                           random_order=True
+                           )
+            ]
+        )
 
     def __len__(self):
         return int(np.ceil(self.num_data / self.batch_size))
@@ -40,6 +63,9 @@ class BatchGenerator(Sequence):
         for image_path in self.image_list[l_bound:r_bound]:
             image = load_image(image_path)
             image = cv2.resize(image, (self.image_shape[0], self.image_shape[1]))
+
+            if self.augment:
+                image = self.aug_pipe.augment_image(image)
             image = normalize(image)
 
             gt = int(image_path.split('/')[-1].split('_')[0])
--- pretrain/train.py
+++ pretrain/train.py
@@ -1,19 +1,40 @@
 from pretrain.alexnet import alex_net
 from pretrain.generator import BatchGenerator
 from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, Callback
+import matplotlib.pyplot as plt
 import os
+
+
+class Histories(Callback):
+    def __init__(self, model, val_gen):
+        self.model = model
+        self.val_gen = val_gen
+        self.losses = []
+        self.val_losses = []
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.losses.append(logs['loss'])
+        val_loss = self.model.evaluate_generator(self.val_gen, len(self.val_gen), workers=12)
+        self.val_losses.append(val_loss[0])
+        print('train: {}, val: {}'.format(self.losses, self.val_losses))
+        plt.plot(self.losses, label='train loss' if epoch == 0 else '', color='r')
+        plt.plot(self.val_losses, label='val loss' if epoch == 0 else '', color='b')
+        plt.legend(loc='upper left')
+        plt.xlabel('Epoch')
+        plt.ylabel('Loss')
+        plt.savefig('./loss.png')
 
 
 def train(train_path, val_path, image_shape, epochs, batch_size, weight_save_path):
     os.makedirs(weight_save_path, exist_ok=True)
     model = alex_net(input_shape=image_shape)
-    model.compile(optimizer=Adam(lr=1e-3), loss='categorical_crossentropy')
+    model.compile(optimizer=Adam(lr=1e-3), loss='categorical_crossentropy', metrics=['acc'])
 
     train_generator = BatchGenerator(train_path, batch_size, image_shape, shuffle=True)
-    val_generator = BatchGenerator(val_path, batch_size, image_shape, shuffle=True)
+    val_generator = BatchGenerator(val_path, batch_size, image_shape, shuffle=True, augmentation=False)
 
-    checkpoint = ModelCheckpoint(filepath=os.path.join(weight_save_path, "yolo_weights.h5"),
+    checkpoint = ModelCheckpoint(filepath=os.path.join(weight_save_path, "alexNet.h5"),
                                  monitor="val_loss",
                                  verbose=1,
                                  save_best_only=True,
@@ -30,7 +51,7 @@ def train(train_path, val_path, image_shape, epochs, batch_size, weight_save_pat
                         verbose=1,
                         validation_data=val_generator,
                         validation_steps=len(val_generator),
-                        callbacks=[checkpoint, reduce_lr],
+                        callbacks=[checkpoint, reduce_lr, Histories(model, val_generator)],
                         shuffle=False)
 
 
