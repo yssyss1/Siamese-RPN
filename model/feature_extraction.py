18172fbcf4ec54d0ba3bb16fe67ebc9e32f29883
seok <seok11754@gmail.com>
2019-03-05 02:31:30 +0900

    modify layers' hyperparameters

--- model/feature_extraction.py
+++ model/feature_extraction.py
@@ -26,13 +26,13 @@ def alexNet(img):
 
     # ======== FREEZE ========
     # PRETRAINED LAYER WITH IMAGENET DATASET
-    x = conv_block(filters=64, kernel_size=11, strides=2, idx=0)(img)
-    x = conv_block(filters=192, kernel_size=5, strides=1, idx=1)(x)
+    x = conv_block(filters=96, kernel_size=11, strides=2, idx=0)(img)
+    x = conv_block(filters=256, kernel_size=5, strides=1, idx=1)(x)
     x = conv_block(filters=384, kernel_size=3, strides=1, idx=2, use_maxpool=False)(x)
     # ======== FREEZE ========
 
-    x = conv_block(filters=256, kernel_size=3, strides=1, idx=3, use_maxpool=False)(x)
-    x = conv_block(filters=256, kernel_size=3, strides=1, idx=4, use_maxpool=False, use_activation=False)(x)
+    x = conv_block(filters=384, kernel_size=3, strides=1, idx=3, use_maxpool=False)(x)
+    x = conv_block(filters=256, kernel_size=3, strides=1, idx=4, use_maxpool=False)(x)
 
     return x
 
@@ -48,5 +48,5 @@ def encoder(input_shape=(None, None, 3)) -> Model:
 
 
 if __name__ == '__main__':
-    encoder_t = encoder(input_shape=(127, 127, 3))
+    encoder_t = encoder(input_shape=(255, 255, 3))
     encoder_t.summary()
