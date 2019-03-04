131ffba04d3f036a35a1e1ac8114e6b4072deaf6
seok <seok11754@gmail.com>
2019-03-05 02:32:01 +0900

    add relu activation function

--- model/rpn.py
+++ model/rpn.py
@@ -1,6 +1,6 @@
 from model.feature_extraction import encoder
 from keras.models import Model
-from keras.layers import Input, Conv2D, Layer, Reshape, Concatenate
+from keras.layers import Input, Conv2D, Layer, Concatenate
 import tensorflow as tf
 from keras.utils import plot_model
 
@@ -25,12 +25,14 @@ class SiameseRPN:
         template_feature = self.feature_extraction(template)
         detection_feature = self.feature_extraction(detection)
 
-        detection_cls_feature = Conv2D(detection_channel_num, kernel_size=3, name='RPN_Detection_CLS_Conv')(detection_feature)
-        detection_reg_feature = Conv2D(detection_channel_num, kernel_size=3, name='RPN_Detection_REG_Conv')(detection_feature)
+        detection_cls_feature = Conv2D(detection_channel_num, kernel_size=3, activation='relu',
+                                       name='RPN_Detection_CLS_Conv')(detection_feature)
+        detection_reg_feature = Conv2D(detection_channel_num, kernel_size=3, activation='relu',
+                                       name='RPN_Detection_REG_Conv')(detection_feature)
 
-        template_cls_feature = Conv2D(filters=2 * self.anchor_num * detection_channel_num, kernel_size=3,
+        template_cls_feature = Conv2D(filters=2 * self.anchor_num * detection_channel_num, kernel_size=3, activation='relu',
                                       name='RPN_Template_CLS_Conv')(template_feature)
-        template_reg_feature = Conv2D(filters=4 * self.anchor_num * detection_channel_num, kernel_size=3,
+        template_reg_feature = Conv2D(filters=4 * self.anchor_num * detection_channel_num, kernel_size=3, activation='relu',
                                       name='RPN_Template_REG_Conv')(template_feature)
 
         # One Shot Detection
@@ -112,40 +114,3 @@ if __name__ == '__main__':
     a = SiameseRPN().build_model()
     a.summary()
     plot_model(a, show_shapes=True)
-
-
-# class SiameseConv(Layer):
-#     """
-#     Custom RPN Layer to compute objectness and box coordinate in anchor
-#     One Shot Detection is used to make online kernel during feed forward
-#     The kernel's weight is extracted from the feature maps of template
-#     """
-#     def __init__(self, branch_name, anchor_num=5, padding='VALID', **kwargs):
-#         self.padding = padding
-#         self.__branch_output = {'cls': 2, 'reg': 4}
-#
-#         if branch_name not in self.__branch_output.keys():
-#             raise ValueError('branch name must be \'cls\' or \'reg\'')
-#
-#         self.branch_name = branch_name
-#         self.output_num = self.__branch_output[branch_name]
-#         self.anchor_num = anchor_num
-#         super(__class__, self).__init__(**kwargs)
-#
-#     def call(self, inputs, **kwargs):
-#         detection = inputs[0]
-#         template = inputs[1]
-#         template_shape = tf.shape(template)
-#
-#         templates = tf.reshape(template, (template_shape[1], template_shape[2], 256, self.output_num * self.anchor_num),
-#                                name='Reshape_{}'.format(self.branch_name))
-#         out = tf.nn.conv2d(detection, templates, strides=(1, 1, 1, 1), padding=self.padding, name='Siamese_Conv_{}'.format(self.branch_name))
-#
-#         return out
-#
-#     def compute_output_shape(self, input_shape):
-#         if self.padding == 'VALID':
-#             return (None, input_shape[0][1] - input_shape[1][1] + 1, input_shape[0][2] - input_shape[1][2] + 1,
-#                     self.output_num * self.anchor_num)
-#         else:
-#             return None, input_shape[0][1], input_shape[0][2], self.output_num, self.anchor_num
